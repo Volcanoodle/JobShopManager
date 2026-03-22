5cfab89591d46594e875303df7848a395c2ed414
Shu Xiangxi <xs4005@hw.ac.uk>
2026-03-22 20:43:18 +0800

    18. test: refactor exampleUR2Test into testUR2

--- src/Tests.java
+++ src/Tests.java
@@ -50,45 +50,33 @@ public class Tests {
 
 
         // UR2 example test
-        public void exampleUR2Test() {
-                //Map<String, Integer> expectedResult = Map.of("FDM",5,"SLA",1);
+        public void testUR2() {
+                System.out.println("\n--- 运行 UR2 测试 ---");
                 JobShopManager jobShopManager = new JobShopManager("FCFS");
 
-                System.out.println("\nStart the machines: \n");
-                //Sart three machines of type FDM and two SLA:
+                System.out.println("启动混合类型的机器 (6台 FDM, 2台 SLA)...");
                 for (int i=1; i<=6; i++) new MachineThread(jobShopManager, "FDM", i).start();
                 for (int i=1; i<=2; i++) new MachineThread(jobShopManager, "SLA", i).start();
-                try {Thread.sleep(100);} catch (InterruptedException e) {e.printStackTrace();} //to allow machine threads to start and run
-
-                //Specify job 1
-                Job job1 = new Job(
-                                "Job1",
+                
+                try {Thread.sleep(200);} catch (InterruptedException e) {e.printStackTrace();} 
+                Job job1 = new Job("Job1",
                                 List.of(new Operation("FDM", 5),
                                         new Operation("FDM", 3),
                                         new Operation("FDM", 3),
                                         new Operation("SLA", 3)));
    
-                //Specify job 2
-                Job job2 = new Job(
-                                "Job2",
+                Job job2 = new Job("Job2",
                                 List.of(new Operation("FDM", 5),
                                         new Operation("FDM", 3)));
 
-                //Print out and submit jobs
-                System.out.println("\nSpecify the Jobs ()   (and note that processing time is not used in FCFS)");
-                System.out.println(job1);
-                System.out.println(job2);     
-                jobShopManager.specifyJobs(List.of(job1, job2));
-                //Allow job specifier to run and release machines
-                try {Thread.sleep(100);} catch (InterruptedException e) {e.printStackTrace();}  
+                System.out.println("多次调用 specifyJobs 提交多个作业...");
+                jobShopManager.specifyJobs(List.of(job1));
+                jobShopManager.specifyJobs(List.of(job2)); 
+                try {Thread.sleep(200);} catch (InterruptedException e) {e.printStackTrace();}  
 
-                System.out.println("\nNow examine the machines released:\n" 
-                        + "\tAs there is no functional code in the JobShopManager yet\n\t" 
-                        + "all six FDM and two SLA machine threads have been released to proceed.\n\t"
-                        + "The correct result would be to release five FDM and one SLA machines after the\n\t"
-                        + "the two jobs had been submitted.\n"
-
-                );    
+                System.out.println("检查结果：");
+                System.out.println("预期行为：控制台上应该共有 5 台 FDM 和 1 台 SLA 打印 'proceeding'，其余 1 台 FDM 和 1 台 SLA 阻塞。");
+                System.out.println("--- UR2 测试结束 ---\n");
         }
 
 }
