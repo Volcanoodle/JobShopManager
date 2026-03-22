64715286b1716e819b34f7ba46061cb74852a922
Shu Xiangxi <xs4005@hw.ac.uk>
2026-03-22 20:06:57 +0800

    test: add UR1 test case

--- src/Tests.java
+++ src/Tests.java
@@ -24,6 +24,31 @@ public class Tests {
         }
 
 
+        public void testUR1() {
+                System.out.println("\n--- 运行 UR1 测试 ---");
+                JobShopManager jobShopManager = new JobShopManager("FCFS");
+
+                System.out.println("启动 5 台 FDM 机器...");
+                for (int i = 1; i <= 5; i++) {
+                        new MachineThread(jobShopManager, "FDM", i).start();
+                }
+                try { Thread.sleep(200); } catch (InterruptedException e) { e.printStackTrace(); }
+
+                Job job1 = new Job(
+                                "Job1",
+                                List.of(new Operation("FDM", 1),
+                                        new Operation("FDM", 1)));
+                System.out.println("提交 1 个作业，需要 2 台 FDM 机器...");
+                jobShopManager.specifyJobs(List.of(job1));
+                
+                try { Thread.sleep(200); } catch (InterruptedException e) { e.printStackTrace(); }  
+
+                System.out.println("检查结果：");
+                System.out.println("预期行为：控制台上应该只有 2 台 FDM 机器打印 'proceeding'，其余 3 台被安全阻塞。");
+                System.out.println("--- UR1 测试结束 ---\n");
+        }
+
+
         // UR2 example test
         public void exampleUR2Test() {
                 //Map<String, Integer> expectedResult = Map.of("FDM",5,"SLA",1);
