c4cdbff211454ba42d641424f4b99c550fb6d229
Shu Xiangxi <xs4005@hw.ac.uk>
2026-03-25 00:30:13 +0800

    refactor: streamline test execution flow in App class

--- src/App.java
+++ src/App.java
@@ -1,12 +1,17 @@
-
-
-// Edit this file 
-
-
-
 public class App {
     public static void main(String[] args) throws Exception {
-        Tests tests =  new Tests();
-		tests.exampleUR2Test();
-	}	
+        Tests tests = new Tests();
+        
+        System.out.println("========== Starting F29OC Coursework Concurrency Tests ==========");
+        
+        tests.testUR1();
+        tests.testUR2();
+        tests.testUR3();
+        tests.testUR4();
+        tests.testUR5();
+        tests.testUR6();
+        
+        System.out.println("========== All Test Cases Executed Successfully ==========");
+        System.exit(0);
+    }	
 }
